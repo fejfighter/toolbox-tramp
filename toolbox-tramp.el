54849b7db11013dde94c3369c1f48c6a3c8201aa
Jeff Walsh <fejfighter@gmail.com>
2022-03-17 00:23:55 +1100

    Package-ify toolbox-tramp

--- toolbox-tramp.el
+++ toolbox-tramp.el
@@ -1,3 +1,32 @@
+;;; toolbox-tramp.el --- tramp connection to toolbx containers  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Jeff Walsh
+
+;; Author: Jeff Walsh <fejfighter@gmail.com>
+;; Keywords: convenience, tools
+;; Version: 0.2.0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Connect to podman's toolbox containers, shamelessly influenced
+;; by/stolen from docker-tramp.el, just specialized/re-written for
+;; toolbox
+
+;;; Code:
+
 (require 'tramp)
 
 (defgroup toolbox-tramp nil
